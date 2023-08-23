2d134a57c31530323606ba73a504df74767eb598
David Aragón <davidaragonandrade@gmail.com>
2023-08-22 23:39:52 -0500

    logic to links

--- src/main.js
+++ src/main.js
@@ -1,5 +1,29 @@
 const iconMenu = document.querySelector('.icon-menu')
 const menu = document.querySelector('.menu')
+const links = document.querySelectorAll('#link')
+
+let linkModified
+
+
+
+window.addEventListener('hashchange', (e) => {
+    changeColorLink(e.newURL)
+})
+
+const changeColorLink = (linkURL) => {
+    let newURL = linkURL.toString().split('#')
+    
+    links.forEach(link => {
+        linkModified = link.textContent.toLocaleLowerCase().split(' ').join('-');
+        
+        if (newURL[1] === linkModified) {
+            link.classList.add('clicked')
+        } else {
+            link.classList.remove('clicked')
+        }
+    })
+    
+}
 
 let openMenu = false
 
