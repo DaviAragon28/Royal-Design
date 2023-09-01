4f73a0cc56f74f9372e35f599f22a990d837e33f
David Aragón <davidaragonandrade@gmail.com>
2023-08-31 21:40:52 -0500

    Logic modal

--- src/main.js
+++ src/main.js
@@ -1,9 +1,22 @@
 const iconMenu = document.querySelector('.icon-menu')
 const menu = document.querySelector('.menu')
+const openModal = document.querySelector('.open-modal')
+const modal = document.querySelector('.modal')
+const closeModal = document.querySelector('.close')
+const videoComplete = document.querySelector('.video-complete')
 const links = document.querySelectorAll('#link')
 
 let linkModified
 
+openModal.addEventListener('click', () => {
+    videoComplete.play()
+    modal.classList.add('opened-modal')
+})
+
+closeModal.addEventListener('click', () => {
+    videoComplete.pause()
+    modal.classList.remove('opened-modal')
+})
 
 
 window.addEventListener('hashchange', (e) => {
@@ -12,17 +25,17 @@ window.addEventListener('hashchange', (e) => {
 
 const changeColorLink = (linkURL) => {
     let newURL = linkURL.toString().split('#')
-    
+
     links.forEach(link => {
         linkModified = link.textContent.toLocaleLowerCase().split(' ').join('-');
-        
+
         if (newURL[1] === linkModified) {
             link.classList.add('clicked')
         } else {
             link.classList.remove('clicked')
         }
     })
-    
+
 }
 
 let openMenu = false
